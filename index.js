037ab680be9b3b301bc73823a7b69d4cb38c8da9
Tarik <tarikboumehdi91@gmail.com>
2025-04-08 21:37:54 -0600

    raillaw deploy link

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 
-const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:5500','https://badrproject-production.up.railway.app'];
+const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:5500','https://bespoke-dieffenbachia-d8a8f5.netlify.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
